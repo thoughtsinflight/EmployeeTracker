1f80ee0a6c14c44d554b076a086ff9dd217d24af
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-12-06 10:54:46 -0500

    another final commit

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
     The purpose of this project is to build an app that with help an Empolyer with employee informantion.
 # Motivation
     My motivation for  completing this project was to become more comfortable with MySQL
+# What I learned
+    I learned more about how to use MySQL and how to organize my code into functions for clearer code. The assignment wasn't terrible difficult but the last bit of functionality I could quite put together.
 # GIF
+![](https://i.imgur.com/X4Fdfv2.gif)
--- schema.sql
+++ schema.sql
@@ -21,3 +21,19 @@ CREATE TABLE department
 id int auto_increment primary key,
 name varchar(30)
 );
+
+INSERT INTO employees (first_name, last_name, role_id) VALUES (`Max`, `Person`, 1);
+INSERT INTO employees (first_name, last_name, role_id) VALUES (`Cass`, `Person`, 2);
+INSERT INTO employees (first_name, last_name, role_id) VALUES (`Jeff`, `Person`, 4);
+INSERT INTO roles (first_name, last_name, role_id) VALUES (`Alex`, `Person`, 3);
+
+INSERT INTO employees (title, salary, department_d) VALUES (`Principle`, 45000, 1);
+INSERT INTO employees (title, salary, department_d) VALUES (`Vice Principle`, 40000, 1);
+INSERT INTO employees (title, salary, department_d) VALUES (`Teacher`, 30000, 3);
+INSERT INTO employees (title, salary, department_d) VALUES (`TA`, 25000, 1);
+
+INSERT INTO department (name) VALUES (`Faculty`);
+INSERT INTO department (name) VALUES (`Math`);
+INSERT INTO department (name) VALUES (`English`);
+INSERT INTO department (name) VALUES (`Science`);
+
